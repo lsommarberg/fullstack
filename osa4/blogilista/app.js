0365f994134ef5eea354f156b95c20bbf728f3db
lsommarberg <laura.sommarberg@gmail.com>
2024-08-02 14:08:49 +0300

    viikko7 tehtävät 1-12

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,46 +1,45 @@
-const config = require('./utils/config')
+const config = require('./utils/config');
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
+app.use(cors());
+app.use(express.json());
 
+const middleware = require('./utils/middleware');
 
-app.use(cors())
-app.use(express.json())
+const logger = require('./utils/logger');
 
-const middleware = require('./utils/middleware')
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message);
+  });
 
-const logger = require('./utils/logger')
+app.use(middleware.tokenExtractor);
 
-mongoose.connect(config.MONGODB_URI)
-.then(() => {
-  logger.info('connected to MongoDB')
-})
-.catch((error) => {
-  logger.error('error connection to MongoDB:', error.message)
-})
+app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
+app.use('/api/blogs', blogsRouter);
 
-app.use(middleware.tokenExtractor)
-
-app.use(middleware.requestLogger)
-
-app.use('/api/login', loginRouter)
-app.use('/api/blogs', blogsRouter)
-
-app.use('/api/users', usersRouter)
+app.use('/api/users', usersRouter);
 
 if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)}
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -1,42 +1,42 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User
-      .find({}).populate("blogs")
-    response.json(users)
-  })
+  const users = await User.find({}).populate('blogs');
+  response.json(users);
+});
 
-  
 usersRouter.post('/', async (request, response, next) => {
-  const { username, name, password } = request.body
+  const { username, name, password } = request.body;
   if (!username || !password) {
-    const error = new Error('Username and password are required')
-    error.name = 'ValidationError'
-    return next(error)
+    const error = new Error('Username and password are required');
+    error.name = 'ValidationError';
+    return next(error);
   }
-  if (username.length <3 || password.length < 3) {
-    const error = new Error('Username and password must be at least 3 characters long')
-    error.name = 'ValidationError'
-    return next(error)
+  if (username.length < 3 || password.length < 3) {
+    const error = new Error(
+      'Username and password must be at least 3 characters long',
+    );
+    error.name = 'ValidationError';
+    return next(error);
   }
-try {
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  try {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
 
-  const savedUser = await user.save()
+    const savedUser = await user.save();
 
-  response.status(201).json(savedUser)
+    response.status(201).json(savedUser);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-module.exports = usersRouter
+module.exports = usersRouter;
